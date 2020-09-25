dc72631fce2e0b0a4191fea7d102bd4b3dea3ea6
Josh Marshall <josh@jmarshall.com.au>
2020-09-25 17:52:47 +1000

    Add id field for label to work

--- src/Materialize.php
+++ src/Materialize.php
@@ -10,39 +10,39 @@ class Materialize extends Form {
         <div class="row">
             <div class="input-field col s12">
                 <?php if ($type == 'submit') { ?>
-                    <input type="submit" name="{{ name }}" value="{{ value }}" class="btn btn-default" {{ readonly }} />
+                    <input type="submit" id="{{ name }}" name="{{ name }}" value="{{ value }}" class="btn btn-default" {{ readonly }} />
                 <?php } else if ($type == 'date') { ?>
-                    <input type="date" name="{{ name }}" value="{{ value }}" class="validate" {{ readonly }} />
+                    <input type="date" id="{{ name }}" name="{{ name }}" value="{{ value }}" class="validate" {{ readonly }} />
                 <?php } else if ($type == 'datetime') { ?>
-                    <input type="datetime-local" name="{{ name }}" value="{{ value }}" class="validate" {{ readonly }} />
+                    <input type="datetime-local" id="{{ name }}" name="{{ name }}" value="{{ value }}" class="validate" {{ readonly }} />
                 <?php } else if ($type == 'time') { ?>
-                    <input type="time" name="{{ name }}" value="{{ value }}" class="validate" {{ readonly }} />
+                    <input type="time" id="{{ name }}" name="{{ name }}" value="{{ value }}" class="validate" {{ readonly }} />
                 <?php } else if ($type == 'color') { ?>
-                    <input type="color" name="{{ name }}" value="{{ value }}" class="validate" {{ readonly }} />
+                    <input type="color" id="{{ name }}" name="{{ name }}" value="{{ value }}" class="validate" {{ readonly }} />
                 <?php } else if ($type == 'email') { ?>
-                    <input type="email" name="{{ name }}" value="{{ value }}" class="validate" {{ readonly }} />
+                    <input type="email" id="{{ name }}" name="{{ name }}" value="{{ value }}" class="validate" {{ readonly }} />
                 <?php } else if ($type == 'bool') { ?>
-                    <label><input type="hidden" name="{{ name }}" value="0" /><input type="checkbox" name="{{ name }}" value="1" {% if value == 1 %}checked{% endif %} {{ readonly }} /><span>{{ label }}</span></label>
+                    <label><input type="hidden" name="{{ name }}" value="0" /><input type="checkbox" id="{{ name }}" name="{{ name }}" value="1" {% if value == 1 %}checked{% endif %} {{ readonly }} /><span>{{ label }}</span></label>
                 <?php } else if ($type == 'booltristate') { ?>
-                    <select name="{{ name }}">
+                    <select id="{{ name }}" name="{{ name }}">
                         <option value="">Neither</option>
                         <option value="1" {% if value == 1 %} selected {% endif %}>Yes</option>
                         <option value="0" {% if value is same as("0") %} selected {% endif %}>No</option>
                     </select>
                 <?php } else if ($type == 'reference') { ?>
-                    <select name="{{ name }}">
+                    <select id="{{ name }}" name="{{ name }}">
                         <option value="">-- None --</option>
                         {{ options|raw }}
                     </select>
                 <?php } else if ($type == 'enum') { ?>
-                    <select name="{{ name }}">
+                    <select id="{{ name }}" name="{{ name }}">
                         <option value="">-- None --</option>
                         {{ options|raw }}
                     </select>
                 <?php } else if ($length > 255 || $length == 0 || $type == 'text') { ?>
-                    <textarea name="{{ name }}" class="materialize-textarea" {{ readonly }}>{{ value|raw }}</textarea>
+                    <textarea id="{{ name }}" name="{{ name }}" class="materialize-textarea" {{ readonly }}>{{ value|raw }}</textarea>
                 <?php } else { ?>
-                    <input type="text" name="{{ name }}" value="{{ value }}" maxlength="<?= $length ?>" data-length="<?= $length ?>" class="validate" {{ readonly }} />
+                    <input id="{{ name }}" type="text" name="{{ name }}" value="{{ value }}" maxlength="<?= $length ?>" data-length="<?= $length ?>" class="validate" {{ readonly }} />
                 <?php } ?>
                 <?php if ($type != 'bool') { ?>
                     <label for="{{ label_for }}">
